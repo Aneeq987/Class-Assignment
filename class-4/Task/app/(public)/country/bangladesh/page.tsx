9c8e2baf8d76ea41d68101887299a3c3f9b33569
Aneeq987 <168265700+Aneeq987@users.noreply.github.com>
2024-10-04 18:01:41 +0500

    Add files via upload

---
+++ class-4/Task/app/(public)/country/bangladesh/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Bangladesh() {
+    return (
+       <>
+       <center>
+       <br />
+       <p style={{fontSize: "20px"}}>Name: Bangladesh</p><br />
+       <p style={{fontSize: "20px"}}>Capital: Dhaka</p><br />
+       <p style={{fontSize: "20px"}}>population: 12121212</p><br />
+       </center>
+       </>
+    )
+}
